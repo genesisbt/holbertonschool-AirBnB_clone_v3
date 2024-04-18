0935781c2a845b6a41c020ff4bc2785043d24c6f
genesisbt <40181430+genesisbt@users.noreply.github.com>
2024-04-18 07:02:37 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -70,12 +70,12 @@ class FileStorage:
         self.reload()
 
     def get(self, cls, id):
-        """Retrieve one object"""
+        """Returns the object based on the class and its ID, or None if not found"""
         key = "{}.{}".format(cls.__name__, id)
         return self.__objects.get(key, None)
 
     def count(self, cls=None):
-        """Count the number of objects in storage"""
+        """Returns the number of objects in storage matching the given class. If no class is passed, returns the count of all objects in storage."""
         if cls:
             count = 0
             for obj in self.__objects.values():
